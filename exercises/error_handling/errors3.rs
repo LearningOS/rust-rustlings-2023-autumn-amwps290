bf9e737740833add69f596d75dea5cfb70051212
amwps290 <huangning0722@163.com>
2023-10-10 22:13:30 +0800

    update

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,11 +7,11 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<(),ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
